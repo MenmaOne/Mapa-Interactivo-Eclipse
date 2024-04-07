f9484ae868e8c94572487517f12974326c527fdc
MenmaOne <bernardolopezortiz4@gmail.com>
2024-04-06 21:47:21 -0600

    archivos

---
+++ Mapa eclipse/mapdata.js
@@ -0,0 +1,345 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "gray",
+    background_transparent: "no",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    "0": {},
+    "1": {},
+    "2": {},
+    "3": {},
+    "4": {},
+    "5": {},
+    "6": {},
+    "7": {},
+    "8": {},
+    "9": {},
+    "10": {},
+    "11": {},
+    "12": {},
+    "13": {},
+    "14": {},
+    "15": {},
+    "16": {},
+    "17": {},
+    "18": {},
+    "19": {},
+    "20": {},
+    "21": {
+      color: "Toluca, Estado de México"
+    },
+    "22": {
+      color: "Torreón, Coahuila"
+    },
+    "23": {
+      color: "Tuxtla Gutiérrez, Chiapas"
+    },
+    "24": {
+      color: "Veracruz, Veracruz"
+    },
+    "25": {
+      color: "Villahermosa, Tabasco"
+    },
+    "26": {
+      color: "Xalapa, Veracruz"
+    },
+    "27": {
+      color: "Zacatecas, Zacatecas"
+    },
+    MEX2706: {
+      name: "Baja California",
+      description: "Tijuana, Baja California - 11:11,  Mexicali, Baja California - 11:13",
+      color: "#fff7c3",
+      hover_color: "#5e5e5e"
+    },
+    MEX2707: {
+      name: "Baja California Sur",
+      description: "La Paz, Baja California Sur - 11:05",
+      color: "#ffa655",
+      hover_color: "#5e5e5e"
+    },
+    MEX2708: {
+      name: "Coahuila",
+      description: "Monclova, Coahuila - 12:24, Piedras Negras, Coahuila - 13:29, Saltillo, Coahuila - 12:22, Torreón, Coahuila - 12:19",
+      color: "#ff0707",
+      hover_color: "#5e5e5e"
+    },
+    MEX2709: {
+      name: "Chihuahua",
+      description: "Chihuahua, Chihuahua - 12:20, Ciudad Juárez, Chihuahua - 12:25",
+      color: "#f7f416",
+      hover_color: "#5e5e5e"
+    },
+    MEX2710: {
+      name: "Durango",
+      description: "Durango, Durango - 12:14, Nazas, Durango - 12:17",
+      color: "#ff0707",
+      hover_color: "#5e5e5e"
+    },
+    MEX2711: {
+      name: "Sinaloa",
+      description: "Culiacán, Sinaloa - 11:11, Mazatlán, Sinaloa - 11:09",
+      color: "#ff0707",
+      hover_color: "#5e5e5e"
+    },
+    MEX2712: {
+      name: "Sonora",
+      description: "Hermosillo, Sonora - 11:13",
+      color: "#f7f416",
+      hover_color: "#5e5e5e"
+    },
+    MEX2713: {
+      name: "Zacatecas",
+      description: "Zacatecas - 12:15",
+      color: "#ffa655",
+      hover_color: "#5e5e5e"
+    },
+    MEX2714: {
+      name: "Nuevo León",
+      description: "Monterrey, Nuevo León - 12:24",
+      color: "#ff0707",
+      hover_color: "#5e5e5e"
+    },
+    MEX2715: {
+      name: "San Luis Potosí",
+      description: "San Luis Potosí, SLP - 12:16",
+      color: "#ffa655",
+      hover_color: "#5e5e5e"
+    },
+    MEX2716: {
+      name: "Tamaulipas",
+      description: "Ciudad Victoria, Tamaulipas - 12:22",
+      color: "#ffa655",
+      hover_color: "#5e5e5e"
+    },
+    MEX2717: {
+      name: "Aguascalientes",
+      description: "Aguascalientes, Aguascalientes - 12:13",
+      color: "#ffa655",
+      hover_color: "#5e5e5e"
+    },
+    MEX2718: {
+      name: "Colima",
+      description: "Colima, Colima - 12:06",
+      color: "#ffa655",
+      hover_color: "#5e5e5e"
+    },
+    MEX2719: {
+      name: "Jalisco",
+      description: "Guadalajara, Jalisco - 12:09, Puerto Vallarta, Jalisco - 12:06",
+      color: "#ff0707",
+      hover_color: "#5e5e5e"
+    },
+    MEX2720: {
+      name: "Michoacán",
+      description: "Morelia, Michoacán - 12:11",
+      color: "#f7f416",
+      hover_color: "#5e5e5e"
+    },
+    MEX2721: {
+      name: "Nayarit",
+      description: "Tepic, Nayarit - 11:08",
+      color: "#ff0707",
+      hover_color: "#5e5e5e"
+    },
+    MEX2722: {
+      name: "Campeche",
+      description: "Campeche, Campeche - 12:31",
+      color: "#fff7c3",
+      hover_color: "#5e5e5e"
+    },
+    MEX2723: {
+      name: "Oaxaca",
+      description: "Oaxaca, Oaxaca - 12:13",
+      color: "#fff7c3",
+      hover_color: "#5e5e5e"
+    },
+    MEX2724: {
+      name: "Puebla",
+      description: "Puebla, Puebla - 12:15",
+      color: "#f7f416",
+      hover_color: "#5e5e5e"
+    },
+    MEX2725: {
+      name: "Tabasco",
+      description: "Villahermosa, Tabasco - 12:22",
+      color: "#fff7c3",
+      hover_color: "#5e5e5e"
+    },
+    MEX2726: {
+      name: "Tlaxcala",
+      description: "Tlaxcala, Tlaxcala - 12:15",
+      color: "#f7f416",
+      hover_color: "#5e5e5e"
+    },
+    MEX2727: {
+      name: "Ciudad de México",
+      description: "Ciudad de México - 12:14",
+      color: "#ffa655",
+      hover_color: "#5e5e5e"
+    },
+    MEX2728: {
+      name: "Guanajuato",
+      description: "Guanajuato, Guanajuato\t12:13, León, Guanajuato - 12:13",
+      color: "#ffa655",
+      hover_color: "#5e5e5e"
+    },
+    MEX2729: {
+      name: "Guerrero",
+      description: "Acapulco, Guerrero - 12:07, Chilpancingo, Guerrero - 12:09",
+      color: "#f7f416",
+      hover_color: "#5e5e5e"
+    },
+    MEX2730: {
+      name: "Hidalgo",
+      description: "Pachuca, Hidalgo - 12:16",
+      color: "#f7f416",
+      hover_color: "#5e5e5e"
+    },
+    MEX2731: {
+      name: "Estado de México",
+      description: "Ecatepec, Estado de México - 12.14, Toluca, Estado de México - 12:13",
+      color: "#f7f416",
+      hover_color: "#5e5e5e"
+    },
+    MEX2732: {
+      name: "Morelos",
+      description: "Cuernavaca, Morelos - 12:13",
+      color: "#f7f416",
+      hover_color: "#5e5e5e"
+    },
+    MEX2733: {
+      name: "Querétaro",
+      description: "Querétaro, Querétaro - 12:14",
+      color: "#f7f416",
+      hover_color: "#5e5e5e"
+    },
+    MEX2734: {
+      name: "Veracruz",
+      description: "Veracruz, Veracruz - 12:19, Xalapa, Veracruz - 12:18",
+      color: "#f7f416",
+      hover_color: "#5e5e5e"
+    },
+    MEX2735: {
+      name: "Chiapas",
+      description: "Tuxtla Gutiérrez, Chiapas - 12:19",
+      color: "#fff7c3",
+      hover_color: "#5e5e5e"
+    },
+    MEX2736: {
+      name: "Quintana Roo",
+      description: "Chetumal, Quintana Roo - 13:33, Cancún, Quintana Roo - 13:41",
+      color: "#fff7c3",
+      hover_color: "#5e5e5e"
+    },
+    MEX2737: {
+      name: "Yucatán",
+      description: "Mérida, Yucatán - 12:35",
+      color: "#fff7c3",
+      hover_color: "#5e5e5e"
+    }
+  },
+
+  labels: {},
+  legend: {
+    entries: [
+      {
+        name: "Visibilidad del 100% - 90%",
+        color: "#ff3c3c",
+        type: "",
+        shape: "square",
+        ids: ""
+      },
+      {
+        name: "Visibilidad del 89% - 70%",
+        color: "#ff902b",
+        type: "",
+        shape: "square",
+        ids: ""
+      },
+      {
+        name: "Visibilidad del 69% - 60%",
+        color: "#f7f416",
+        type: "",
+        shape: "square",
+        ids: ""
+      },
+      {
+        name: "Visibilidad menor a 60%",
+        color: "#fff7c3",
+        type: "",
+        shape: "square",
+        ids: ""
+      }
+    ]
+  },
+  regions: {}
+};
